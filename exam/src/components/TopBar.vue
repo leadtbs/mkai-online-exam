9787913a8a42276a95cc0306af1bbdd99d6557fa
Benedict Badilles <benbenbadilles@gmail.com>
2020-05-08 10:14:46 +0800

    before working on cbt nursing

--- exam/src/components/TopBar.vue
+++ exam/src/components/TopBar.vue
@@ -2,7 +2,7 @@
     <div>
         <b-navbar toggleable="lg" type="dark" style="background-color: #00A0EA;">
             <b-navbar-nav><b-nav-item :to="{name: 'home'}">Home</b-nav-item></b-navbar-nav>
-            <b-navbar-brand class="mx-auto" href="#">MKAI Online Exam <b-badge pill variant="success">BETA</b-badge></b-navbar-brand>
+            <b-navbar-brand class="mx-auto" href="#">MKAI Computer Based Test <b-badge pill variant="success">BETA</b-badge></b-navbar-brand>
             <template v-if="authenticated">      
                 <b-navbar-nav><b-nav-item href="#" @click.prevent="signOut">Logout</b-nav-item></b-navbar-nav>       
             </template>
